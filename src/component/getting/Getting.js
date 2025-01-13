65fb0c4a5b2bc6e2109a58f8a05afefc7e29494d
Albert <albert.svetlov.1993@mail.ru>
2025-01-13 23:58:50 +0300

    Доработал карточки с описанием Авторов

--- src/component/getting/Getting.js
+++ src/component/getting/Getting.js
@@ -25,17 +25,38 @@ const Getting = () =>{
 const View = ({result}) =>{
         
             const element = result.map((item,i) =>{
+                const {name,videoUrl,thumbs,title,text,skills } = item
+                const nameAvtor = item.name.split(' ').slice(-1)
+                
                 return(
                     <div class='getting__item' key={i}>
                         <div class='getting__top'>
-                            <img src={item.thumbs} alt="alt" />
+                            <img src={thumbs} alt="alt" />
                             <div class='getting__block'>
-                                <BtnAnimation link={item.videoUrl}/>
-                                {item.name}
+                                <BtnAnimation link={videoUrl}/>
+                                {name}
                             </div>
                         </div>
                         <div class='getting__bootom'>
-        
+                            <div class='getting__sub-top'>
+                                <h3 class='getting__nameTitle'>{title}</h3>
+                                <p class='getting__name'>{nameAvtor}</p>
+                                <p class='getting__text'>{text}</p>
+                            </div>
+                            <div class='getting__block-skill'>
+                                <h3 class='getting__skills-title'>Skills</h3>
+                                <div class='getting__icons'>
+                                    {skills.map((item,i) => {
+                                        return(                                            
+                                            <div class='getting__block-icon' key={i}>
+                                                <div class='getting__icon'><img src={item.icon} alt={item.name}/></div>
+                                                <p class='getting__name-skill'>{item.name}</p>
+                                            </div>
+                                        )
+                                    })}
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 )
--- src/component/service/Service.js
+++ src/component/service/Service.js
@@ -13,3 +13,4 @@ class Server {
 }
 
 export default Server
+
